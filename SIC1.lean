a1ae33cf0710816268e2794f8f586e8dbf5fe5c4
Will Bradley <68962413+a2435191@users.noreply.github.com>
2025-11-28 18:42:52 -0500

    Create LawfulApplicative instance for `ProgramIO`

--- SIC1.lean
+++ SIC1.lean
@@ -46,45 +46,169 @@ inductive ProgramIO.Read? (α : Type u)
 | readIn (ofData : Data → α)
 deriving Inhabited
 
+/-- Halt or write to `OUT` or read from `IN`. -/
 inductive ProgramIO (α : Type u)
-/-- Don't write anything, just possibly read. -/
-| noWrite : ProgramIO.Read? α → ProgramIO α
+/-- Don't write anything, just possibly read from `IN`. -/
+| justRead : ProgramIO.Read? α → ProgramIO α
 /-- After possibly reading from `IN`, write some `Data` to `OUT` and return some `α`. -/
 | writeOut : ProgramIO.Read? (Data × α) → ProgramIO α
 /-- Halt the program. -/
 | halted
 deriving Inhabited
 
--- inductive ProgramIO (α : Type u)
---
--- | none : ProgramIO.Terminal Unit α → ProgramIO α
--- /-- Wait on a value from `IN` and then perform a terminal action. -/
--- | readIn : ProgramIO.Terminal Data α → ProgramIO α
--- deriving Inhabited
-
 namespace ProgramIO
 
-def pure (a : α) : ProgramIO α :=
-  .noWrite (.none a)
-
-namespace Read?
-
-def map (f : α → β) : Read? α → Read? β
+@[simp]
+def Read?.map (f : α → β) : Read? α → Read? β
 | .none a => .none (f a)
 | .readIn g => .readIn (f ∘ g)
 
 instance : Functor Read? where
-  map := map
+  map := .map
 
-end Read?
+theorem Read?.map_eq : Read?.map f mx = f <$> mx := rfl
 
-def map (f : α → β) : ProgramIO α → ProgramIO β
-| .noWrite r => .noWrite (f <$> r)
-| .writeOut r => .writeOut (Prod.map id f <$> r)
-| .halted => .halted
+instance : LawfulFunctor Read? where
+  map_const := rfl
+  id_map := by intros; dsimp [Functor.map]; split <;> rfl
+  comp_map := by intros; dsimp [Functor.map]; split <;> rfl
+
+@[simp]
+def Read?.seq : Read? (α → β) → (Unit → Read? α) → Read? β :=
+  fun mf mx =>
+    let mx := mx ()
+    match mf, mx with
+    | .none f, .none x => .none (f x)
+    | .none f, .readIn xNext => .readIn (f ∘ xNext)
+    | .readIn fNext, .none x => .readIn (fNext · x)
+    | .readIn fNext, .readIn xNext =>
+      .readIn fun d => fNext d (xNext d)
+
+instance : Applicative Read? where
+  pure := .none
+  seq := .seq
+
+theorem Read?.pure_eq : Read?.none a = pure a := rfl
+theorem Read?.seq_eq : Read?.seq mf mx = mf <*> (mx ()) := rfl
+
+instance : LawfulApplicative Read? where
+  seqLeft_eq := by intros; rfl
+  seqRight_eq := by intros; rfl
+  pure_seq := by
+    intros
+    dsimp [Seq.seq, Functor.map, pure]
+    split <;> simp_all
+  map_pure := by intros; rfl
+  seq_pure := by
+    intros
+    dsimp [Seq.seq, Functor.map, pure]
+    split <;> simp_all <;> funext; dsimp
+  seq_assoc := by
+    intros
+    simp only [Seq.seq, Functor.map, Read?.seq, Read?.map]
+    (repeat' split at *)
+    <;> simp_all only [
+      Read?.readIn.injEq, Read?.none.injEq,
+      Function.comp_apply, reduceCtorEq]
+    <;> (rename_i h; rewrite [←h]; rfl)
 
 instance : Functor ProgramIO where
-  map := map
+  map f
+  | .justRead r => .justRead (f <$> r)
+  | .writeOut r => .writeOut ((fun (d, a) => (d, f a)) <$> r)
+  | .halted => .halted
+
+instance : LawfulFunctor ProgramIO where
+  map_const := rfl
+  id_map := by
+    intros
+    dsimp [Functor.map]
+    (repeat' split) <;> rfl
+  comp_map := by
+    intros
+    dsimp [Functor.map]
+    (repeat' split) <;> rfl
+
+def _root_.Applicative.map₂ [Applicative m] (ma : m α) (mb : m β) (f : α → β → γ) : m γ :=
+  f <$> ma <*> mb
+
+instance : Applicative ProgramIO where
+  pure a := .justRead (.none a)
+  seq mf ma := match mf with
+    | .halted => .halted
+    | .justRead rf =>
+      match ma () with
+      | .halted => .halted
+      | .justRead ra => .justRead (rf <*> ra)
+      | .writeOut ra => .writeOut <|
+        Applicative.map₂ rf ra fun f (d, a) => (d, f a)
+    | .writeOut rf =>
+      match ma () with
+      | .halted => .halted
+      | .justRead ra => .writeOut <|
+        Applicative.map₂ rf ra fun (d, f) a => (d, f a)
+      | .writeOut ra => .writeOut <|
+        -- Overwrite the previously written data
+        Applicative.map₂ rf ra fun (_, f) (d, a) => (d, f a)
+
+section
+
+variable {α : Type u} {β : Type v}
+
+def prodMapR : Data × (α → β) → α → Data × β :=
+  fun (d, f) a => (d, f a)
+
+theorem eq_prodMapR : (fun x a => (x.1, x.2 a)) = @prodMapR α β :=
+  rfl
+
+def prodMapL : (α → β) → Data × α → Data × β :=
+  fun f (d, a) => (d, f a)
+
+theorem eq_prodMapL : (fun x a => (a.1, x a.2)) = @prodMapL α β :=
+  rfl
+
+def prodMap' : Data × (α → β) → Data × α → Data × β :=
+  fun (_, f) (d, a) => (d, f a)
+
+theorem eq_prodMap' : (fun x a => (a.1, x.2 a.2)) = @prodMap' α β :=
+  rfl
+
+end
+
+instance : LawfulApplicative ProgramIO where
+  seqLeft_eq := by intros; rfl
+  seqRight_eq := by intros; rfl
+  pure_seq := by
+    rintro α β f (mx|mx)
+    all_goals dsimp [pure, Seq.seq, Functor.map, -Read?.seq]
+    all_goals
+      congr
+      -- We already showed this in the `LawfulApplicative ProgramIO.Read?` instance
+      show pure _ <*> mx = _ <$> mx
+      apply pure_seq
+  map_pure := by intros; rfl
+  seq_pure := by
+    rintro α β (mf|mf) a
+    all_goals
+      dsimp [pure, Seq.seq, Functor.map, -Read?.seq, -Read?.map]
+      <;> (congr 1; show _ <*> pure a = _ <$> mf)
+    · apply seq_pure
+    · rw [seq_pure]
+      dsimp [Functor.map] <;> (repeat' split at *) <;> simp_all
+      <;> (rename_i h; rw [←h]) <;> rfl
+  seq_assoc := by
+    simp only [Seq.seq, Functor.map, Applicative.map₂]
+    rintro α β γ (ma|ma) (mf|mf) (mg|mg) <;> try rfl
+    all_goals
+      (simp only <;> congr 1)
+      simp only [Read?.map_eq, Read?.seq_eq]
+      try simp only [eq_prodMapR, eq_prodMapL, eq_prodMap']
+    any_goals rw [←seq_assoc]
+    all_goals
+      repeat rw [←pure_seq]
+      simp only [seq_assoc, map_pure, seq_pure]
+      simp only [pure_seq, Functor.map_map]
+      rfl
 
 end ProgramIO
 
@@ -144,13 +268,13 @@ def State.step : State → ProgramIO State
 
   -- Write `res := mem[A] - mem[B]` to address `A`
   if h : a ≤ .max then
-    .noWrite <| memAB.apply fun res instrAddress' =>
+    .justRead <| memAB.apply fun res instrAddress' =>
       ⟨mem.setByte a res h, instrAddress'⟩
   else
     match a with
     | .halt => .halted
     | .in =>
-      .noWrite <| memAB.apply fun _ instrAddress' =>
+      .justRead <| memAB.apply fun _ instrAddress' =>
         ⟨mem, instrAddress'⟩
     | .out =>
       .writeOut <| memAB.apply fun res instrAddress' =>
@@ -167,8 +291,8 @@ where go (inputs acc : List Data) fuel (state : State) :=
     let io := state.step
     match io with
     | .halted => acc
-    | .noWrite (.none state') => go inputs acc k state'
-    | .noWrite (.readIn next) =>
+    | .justRead (.none state') => go inputs acc k state'
+    | .justRead (.readIn next) =>
       match inputs with
       | [] => acc
       | inp :: inputs' => go inputs' acc k (next inp)
@@ -226,7 +350,7 @@ instance [ToString α] : ToString (ProgramIO α) where
   toString
   | .halted => ".halted"
   | .writeOut r => s!".writeOut <| {r}"
-  | .noWrite r => s!".noWrite <| {r}"
+  | .justRead r => s!".justRead <| {r}"
 
 instance : ToString State where
   toString | { mem, idx } => s!"⟨{mem}, {idx}⟩"
