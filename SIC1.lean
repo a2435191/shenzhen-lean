87d2d3fc1a5d8da15aced4285eba6ab35dd57890
Will Bradley <68962413+a2435191@users.noreply.github.com>
2025-11-28 13:52:33 -0500

    Switch SIC `ProgramIO` type to non-monadic structure

--- SIC1.lean
+++ SIC1.lean
@@ -18,65 +18,73 @@ namespace Index
 end Index
 
 abbrev Mem.size := 256
+@[simp] abbrev Mem.accessibleSize := 253
 
 deriving instance Repr for ByteArray
 
 structure Mem where
   bytes : ByteArray
-  hsize : bytes.size = Mem.size := by decide
-  hzero : ∀ (i : Index), i > .max → (bytes.get i.toNat (hsize ▸ i.toNat_lt_size)) = 0
-    := by decide
+  hsize : bytes.size = Mem.accessibleSize := by decide
 deriving Repr
 
 namespace Mem
 
 /-- All zeroes -/
 def default : Mem where
-  bytes := ⟨Array.replicate size 0⟩
+  bytes := ⟨Array.replicate accessibleSize 0⟩
   hsize := by simp [ByteArray.size]
-  hzero := by simp [ByteArray.get]
 
 instance : Inhabited Mem := ⟨default⟩
 
 end Mem
 
-inductive ProgramIO.Terminal (α : Type u)
-| pure : α → ProgramIO.Terminal α
-| writeOut (d : Data) (a : α)
-| halted
+/-- Possibly read from `IN`. -/
+inductive ProgramIO.Read? (α : Type u)
+/-- Don't read any value from `IN`. -/
+| none (a : α)
+/-- Wait on a value from `IN`. -/
+| readIn (ofData : Data → α)
+deriving Inhabited
 
 inductive ProgramIO (α : Type u)
-| terminal : ProgramIO.Terminal α → ProgramIO α
-/-- Waiting on a value from input. -/
-| readIn (next : Data → ProgramIO α)
+/-- Don't write anything, just possibly read. -/
+| noWrite : ProgramIO.Read? α → ProgramIO α
+/-- After possibly reading from `IN`, write some `Data` to `OUT` and return some `α`. -/
+| writeOut : ProgramIO.Read? (Data × α) → ProgramIO α
+/-- Halt the program. -/
+| halted
+deriving Inhabited
+
+-- inductive ProgramIO (α : Type u)
+--
+-- | none : ProgramIO.Terminal Unit α → ProgramIO α
+-- /-- Wait on a value from `IN` and then perform a terminal action. -/
+-- | readIn : ProgramIO.Terminal Data α → ProgramIO α
+-- deriving Inhabited
 
 namespace ProgramIO
 
-def pure (a : α) :=
-  terminal (.pure a)
+def pure (a : α) : ProgramIO α :=
+  .noWrite (.none a)
 
-def writeOut (d : Data) (a : α) :=
-  terminal (.writeOut d a)
+namespace Read?
 
-def halted : ProgramIO α :=
-  terminal .halted
+def map (f : α → β) : Read? α → Read? β
+| .none a => .none (f a)
+| .readIn g => .readIn (f ∘ g)
 
-def writeIfNotAlreadyWritten (toWrite : Data) : ProgramIO α → ProgramIO α
-| .pure a => .writeOut toWrite a
-| .readIn next => .readIn fun toRead =>
-  writeIfNotAlreadyWritten toWrite (next toRead)
-| .writeOut d a => .writeOut d a -- ignore write in this case
+instance : Functor Read? where
+  map := map
+
+end Read?
+
+def map (f : α → β) : ProgramIO α → ProgramIO β
+| .noWrite r => .noWrite (f <$> r)
+| .writeOut r => .writeOut (Prod.map id f <$> r)
 | .halted => .halted
 
-def bind : ProgramIO α → (α → ProgramIO β) → ProgramIO β
-| .pure a, f => f a
-| .readIn next, f => .readIn fun d => bind (next d) f
-| .writeOut d a, f => writeIfNotAlreadyWritten d (f a) -- don't overwrite the write-out of `f a`, if it exists
-| .halted, _ => .halted
-
-instance : Monad ProgramIO where
-  pure := pure
-  bind := bind
+instance : Functor ProgramIO where
+  map := map
 
 end ProgramIO
 
@@ -87,80 +95,90 @@ deriving Inhabited, Repr
 
 namespace Mem
 
-theorem index_valid {idx : Index} : idx.toNat < size :=
-  idx.toNat_lt_size
-
-theorem index_valid' {idx : Index} {mem : Mem} : idx.toNat < mem.bytes.size :=
-  mem.hsize ▸ index_valid
-
 def getByte (mem : Mem) (idx : Index) : Data :=
-  (mem.bytes.get idx.toNat index_valid').toInt8
+  if h : idx.toNat < Mem.accessibleSize then
+    (mem.bytes.get idx.toNat (mem.hsize ▸ h)).toInt8
+  else 0
 
-def read (mem : Mem) (idx : Index) : ProgramIO Data :=
-  match idx with
-  | .in => .readIn pure
-  | .out => pure 0
-  | .halt => .halted
-  | _ => pure (mem.getByte idx)
+theorem le_max_iff {i : Index} : i ≤ .max ↔ i < 253 := by
+  rw [Index.max, UInt8.le_iff_toNat_le, UInt8.lt_iff_toNat_lt]
+  exact Nat.lt_add_one_iff.symm
 
 def setByte (mem : Mem) (i : Index) (val : Data) (h : i ≤ .max := by decide) : Mem where
-  bytes := mem.bytes.set i.toNat val.toUInt8 index_valid'
+  bytes := mem.bytes.set i.toNat val.toUInt8 <| by
+    rwa [mem.hsize, show accessibleSize = UInt8.toNat 253 by rfl,
+        ←UInt8.lt_iff_toNat_lt, ←le_max_iff]
   hsize := by
-    simp [ByteArray.set, ByteArray.size]
-    exact mem.hsize
-  hzero := by
-    intro j hj
-    have := mem.hzero j hj
-    simp [ByteArray.set, ByteArray.get] at ⊢ this
-    rw [←this]
-    apply Array.getElem_set_ne
-    apply Nat.ne_of_lt
-    rw [←UInt8.lt_iff_toNat_lt]
-    exact UInt8.lt_of_le_of_lt h hj
+    rw [ByteArray.size, ByteArray.set, Array.size_set,
+        ←ByteArray.size, mem.hsize]
 
-def write (mem : Mem) (idx : Index) (val : Data) : ProgramIO Mem :=
-  if h₁ : idx = .in then pure mem
-  else if h₂ : idx = .out then .writeOut val mem
-  else if h₃ : idx = .halt then .halted
-  else pure (setByte mem idx val (by simp at *; grind))
+instance : ToString Mem where
+  toString | { bytes, .. } => toString bytes
 
 end Mem
 
-def State.step (state : State) : ProgramIO State := do
-  let ⟨mem, instrAddress⟩ := state
+def State.step : State → ProgramIO State
+| ⟨mem, instrAddress⟩ =>
   let a : Index := Int8.toUInt8 (mem.getByte instrAddress)
   let b : Index := Int8.toUInt8 (mem.getByte (instrAddress + 1))
   let c : Index := Int8.toUInt8 (mem.getByte (instrAddress + 2))
-  let (memA, memB) ← (match a, b with
-    | .in, .in => do
+
+  let memAB : ProgramIO.Read? (Data × Data) :=
+    match a, b with
+    | .in, .in =>
       /- erratum #4: reading from `@IN` multiple times
-      only consumes a single input -/
-      let input ← mem.read .in
-      pure (input, input)
-    | _, _ => do pure (←mem.read a, ←mem.read b))
+        only consumes a single input -/
+      .readIn fun x => (x, x)
+    | .in, _ =>
+      .readIn (·, mem.getByte b)
+    | _, .in =>
+      .readIn (mem.getByte a, ·)
+    | _, _ =>
+      .none (mem.getByte a, mem.getByte b)
 
-  let mem' ← mem.write a (memA - memB)
-  let instrAddress' := if memA ≤ memB then c else (instrAddress + 3)
+  let memAB.apply {α : Type} (fn : Data → Index → α) : ProgramIO.Read? α :=
+    memAB <&> fun (memA, memB) =>
+      let res := memA - memB
+      let instrAddress' := if res ≤ 0 then c else instrAddress + 3
+      fn res instrAddress'
 
-  return { mem := mem', idx := instrAddress' }
+  -- Write `res := mem[A] - mem[B]` to address `A`
+  if h : a ≤ .max then
+    .noWrite <| memAB.apply fun res instrAddress' =>
+      ⟨mem.setByte a res h, instrAddress'⟩
+  else
+    match a with
+    | .halt => .halted
+    | .in =>
+      .noWrite <| memAB.apply fun _ instrAddress' =>
+        ⟨mem, instrAddress'⟩
+    | .out =>
+      .writeOut <| memAB.apply fun res instrAddress' =>
+        ⟨res, mem, instrAddress'⟩
+    | _ => unreachable!
 
-def State.eval (inputs : List Data) (init : ProgramIO State) (maxIters : Nat := 1024) : List Data :=
+def State.eval (inputs : List Data) (init : State) (maxIters : Nat := 1024) : List Data :=
   (go inputs [] maxIters init).reverse
-where go inputs (acc : List Data) fuel (io : ProgramIO State) :=
+where go (inputs acc : List Data) fuel (state : State) :=
   match fuel with
-  | 0 => acc
+  | 0 =>
+    acc
   | k + 1 =>
+    let io := state.step
     match io with
     | .halted => acc
-    | .pure state =>
-      go inputs acc k (step state)
-    | .readIn next =>
+    | .noWrite (.none state') => go inputs acc k state'
+    | .noWrite (.readIn next) =>
       match inputs with
       | [] => acc
-      | hd :: tl =>
-        go tl acc k (next hd)
-    | .writeOut d state =>
-      go inputs (d :: acc) k (step state)
+      | inp :: inputs' => go inputs' acc k (next inp)
+    | .writeOut (.none (d, state')) => go inputs (d :: acc) k state'
+    | .writeOut (.readIn next) =>
+      match inputs with
+      | [] => acc
+      | inp :: inputs' =>
+        let (d, state') := next inp
+        go inputs' (d :: acc) k state'
 
 def Mem.ofList (bytes : List Data) (h : bytes.length ≤ Index.max.toNat + 1 := by decide) : Mem :=
   bytes
@@ -175,7 +193,7 @@ def Mem.ofList (bytes : List Data) (h : bytes.length ≤ Index.max.toNat + 1 := 
         exact this
       mem.setByte i d this
 
-def Mem.ofString (s : String) := do
+def Mem.ofString? (s : String) : Option Mem := do
   let bytes ← s.splitOn "\t"
     |>.mapM (fun
       | String.mk [c₁, c₂] => do
@@ -194,9 +212,27 @@ where
       some (c.toUInt8 - 'A'.toUInt8 + 10)
     else none
 
+def prog :=
+  "00	fd	03	00	fd	06	fe	00	09	00	00	00	00	00	00	00"
+  |> Mem.ofString?
+  |> Option.get!
+
+instance [ToString α] : ToString (ProgramIO.Read? α) where
+  toString
+  | .none a => s!".none {a}"
+  | .readIn f => s!".readIn fun | 37 => {f 37} | _ => …"
+
+instance [ToString α] : ToString (ProgramIO α) where
+  toString
+  | .halted => ".halted"
+  | .writeOut r => s!".writeOut <| {r}"
+  | .noWrite r => s!".noWrite <| {r}"
+
+instance : ToString State where
+  toString | { mem, idx } => s!"⟨{mem}, {idx}⟩"
+
 #eval do
-  let prog ← Mem.ofString "00	fd	03	00	fd	06	fe	00	09	00	00	00	00	00	00	00"
   let initialState := State.mk prog 0
   let inputs := [1, 1, 1, 2, 1, -1, 11, 25, 82, 17]
-  let result := State.eval inputs (pure initialState)
+  let result := State.eval inputs initialState
   return result
