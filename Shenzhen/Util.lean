ac27590735bf13dc0020b1e2e32384fcf215a76c
Will Bradley <68962413+a2435191@users.noreply.github.com>
2025-12-12 02:44:26 -0500

    Complete `nextFinIdx?`

--- Shenzhen/Util.lean
+++ Shenzhen/Util.lean
@@ -56,66 +56,56 @@ theorem add_sub_cancel {a b : Fin n} : a + (b - a) = b :=
   have := instNeZero
   _root_.add_sub_cancel a b
 
+/-- Find the smallest `j > i` s.t. `p j = true`, or `none` if no such `j` exists. -/
+@[specialize] def nextFinIdx? (i : Fin n) (p : Fin n → Bool) : Option (Fin n) :=
+  if h : i.val + 1 < n then
+    letI iSucc := mk (i + 1) h
+    if p iSucc then some iSucc else nextFinIdx? iSucc p
+  else none
+termination_by n - i.val
+
+theorem nextFinIdx?_eq {i : Fin n} : nextFinIdx? i p = find? fun j => j > i && p j := by
+  match n with
+  | 0 => exact i.elim0
+  | n' + 1 =>
+    induction i using reverseInduction with
+    | last =>
+      convert_to none = none
+      · simp [nextFinIdx?]
+      · simp [le_last]
+      · rfl
+    | cast i' ih =>
+      have : i'.castSucc.val + 1 < n' + 1 := by
+        simp
+      unfold nextFinIdx? at ⊢
+      simp only [this, dite_true]
+      split_ifs with h
+      · refine (find?_eq_some_iff.mpr ⟨?_, ?_⟩).symm
+        · exact Bool.and_eq_true_iff.mpr ⟨by simp [lt_def], h⟩
+        · intro ⟨j, hj₁⟩ hj₂
+          rw [Bool.and_eq_false_iff, decide_eq_false_iff_not, gt_iff_lt]
+          left
+          simpa [le_def, Nat.lt_succ_iff] using hj₂
+      · show i'.succ.nextFinIdx? p = _
+        simp_rw [ih, lt_def, val_succ, coe_castSucc]
+        congr 1
+        funext j
+        if h₁ : i'.val + 1 < j.val then
+          have : i'.val < j.val := by omega
+          simp only [h₁, this]
+        else if h₂ : j.val ≤ i'.val then
+          have this₁ : ¬(i'.val + 1 < j) := by omega
+          have this₂ : ¬(i'.val < j) := by omega
+          simp_rw [this₁, this₂]
+        else
+          have : j.val = i' + 1 := by omega
+          simp_rw [this, lt_self_iff_false, Nat.lt_succ_self,
+                   decide_false, Bool.false_and,
+                   decide_true, Bool.true_and]
+          symm
+          simp only [coe_castSucc, Bool.not_eq_true] at h
+          convert h
 end Fin
-
-namespace Vector
-@[specialize] def nextFinIdx? (vec : Vector α n) (i : Fin n) (p : Fin n → α → Bool) : Option (Fin n) :=
-  (go 0).1
-where go (δ : Fin (n + 1)) : { o : Option (Fin n) // o.isSome ↔ ∃ k : Fin δ.rev, p (i + k.castLE δ.rev.is_le) vec[i + k.castLE δ.rev.is_le] } :=
-  if h : δ = Fin.last n then ⟨none, by simp; intro ⟨_, h'⟩; simp [h] at h'⟩
-  else
-    have := Fin.val_lt_last h -- for termination proof
-    let castδ : Fin n := ⟨δ, this⟩
-    let j := i + castδ
-    if h' : p j vec[j] then ⟨j, by simp; sorry⟩ else
-      let ⟨next, hnext⟩ := go castδ.succ
-      ⟨next, sorry⟩
-
--- #check
--- #check Int.add_sub
--- lemma nextFinIdx?_isSome_iff {i : Fin n} : (nextFinIdx? v i p).isSome ↔ ∃ j : Fin n, p j v[j] := by
---   suffices h : ∀ δ : Fin (n + 1), (nextFinIdx?.go v i p δ.rev).isSome ↔
---       ∃ k : Fin δ, p (i + k.castLE δ.is_le) v[i + k.castLE δ.is_le] from by
---     convert h ⟨n, Nat.lt_add_one n⟩ using 1
---     rw [nextFinIdx?, Bool.coe_iff_coe]
---     congr
---     · exact (Fin.rev_last n).symm
---     · constructor
---       · rintro ⟨j, hj⟩
---         use j - i
---         simpa [Fin.add_sub_cancel]
---       · rintro ⟨k, hk⟩
---         use i + k
---         simpa using hk
---   rintro ⟨δ, hδ⟩
---   induction δ with
---   | zero => simp [nextFinIdx?.go]
---   | succ k ih =>
---     unfold nextFinIdx?.go
---     split <;> rename_i h
---     · simp [Fin.rev_eq_iff] at h
---     · have := Fin.val_lt_last h
---       simp only
---       let castδ := Fin.mk (Fin.rev ⟨k + 1, hδ⟩) this
---       refold_let castδ
---       have hδ' : k < n + 1 := Nat.lt_of_succ_lt hδ
---       have : castδ.succ = Fin.rev ⟨k, hδ'⟩ := by
---         simp [castδ, Fin.rev]
---         omega
---       simp [this] at *
---       split
---       · simp only [Option.isSome_some, true_iff]
---         use ⟨castδ, sorry⟩
---         simp_all
---       · simp [ih hδ']
-
---         sorry
--- @[specialize, inline] def nextFinIdx (vec : Vector α n) (i : Fin n) (p : Fin n → α → Bool)
---     (h : ∃ j : Fin n, p j vec[j]) : Fin n :=
---   (vec.nextFinIdx? i p).get <| by
---     sorry
-
-end Vector
 
 @[always_inline]
 def Fin.succ' : Fin n → Fin n
