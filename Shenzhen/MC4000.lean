b2bd15e130fe434e0b3e459c18da4db6c9f06735
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-05-24 03:47:58 -0400

    Rename `mask` to `alreadyTicked` and invert it

--- Shenzhen/MC4000.lean
+++ Shenzhen/MC4000.lean
@@ -319,40 +319,40 @@ end State
 -/
 
 /-- Try to resolve the top-level (outermost) XBus reads and peeks with writes for which the
-  waiting-to-write flag has been set and `mask` is `true`. If there are multiple such writers,
+  waiting-to-write flag has been set and `alreadyTicked` is `false`. If there are multiple such writers,
   the order is unspecified (but really left-to-right in `states`).
-  `mask[i] = false` indicates that `states[i]` has already been ticked and should be ignored. -/
+  `alreadyTicked[i] = true` indicates that `states[i]` has already been ticked and should be ignored. -/
 def resolveXBusReadsAndPeeks {n : ℕ} (xBusConns : Conns n XBus)
-    (states : Vector State n) (mask : Vector Bool n) : Vector State n × Vector Bool n :=
-  (List.finRange n).foldl (init := (states, mask)) fun (states, mask) i =>
-    if !mask[i] then (states, mask)
+    (states : Vector State n) (alreadyTicked : Vector Bool n) : Vector State n × Vector Bool n :=
+  (List.finRange n).foldl (init := (states, alreadyTicked)) fun (states, alreadyTicked) i =>
+    if alreadyTicked[i] then (states, alreadyTicked)
     else
       match states[i].instructionState with
       | .xBusRead pin next =>
-        match findWrite? (i, pin) states mask with
+        match findWrite? (i, pin) states alreadyTicked with
         | .some ⟨j, _, d, next'⟩ =>
           let states' := states
             |>.set i { states[i] with instructionState := next d }
             |>.set j { states[j] with instructionState := next' () }
-          (states', mask) -- Don't update mask— we might have more "free" operations (second bullet point below) to do
-        | none => (states, mask.set i false) -- update mask since this read blocks, meaning we're done for the tick
+          (states', alreadyTicked) -- Don't update mask— we might have more "free" operations (second bullet point below) to do
+        | none => (states, alreadyTicked.set i true) -- update mask since this read blocks, meaning we're done for the tick
       | .xBusPoll pin next =>
-        match findWrite? (i, pin) states mask with
+        match findWrite? (i, pin) states alreadyTicked with
         | .some ⟨j, _, _, next'⟩ =>
           let states' := states
             |>.set i { states[i] with instructionState := next () }
             -- don't resolve the write since this is just a poll
-          (states', mask)
-        | none => (states, mask.set i false) -- update mask since this poll blocks
-      | _ => (states, mask)
+          (states', alreadyTicked)
+        | none => (states, alreadyTicked.set i true) -- update mask since this poll blocks
+      | _ => (states, alreadyTicked)
 where
   /-- Compute the first index of an XBus write in `states` s.t. it is on a pin connected to `whichPin`,
-    its `mask` bit is set, and its `waiting-to-write` flag is set.
+    its `alreadyTicked` bit is not set, and its `waiting-to-write` flag is set.
     Also return the `(outPin, d, next)` arguments to the `.xBusWrite` constructor. -/
-  findWrite? (whichPin : Fin n × XBus) (states : Vector State n) (mask : Vector Bool n)
+  findWrite? (whichPin : Fin n × XBus) (states : Vector State n) (alreadyTicked : Vector Bool n)
       : Option ((j : Fin n) × XBus × Integer × (Unit → IOEffects XBus SimpleIO (InstructionState states[j].m))) :=
     Fin.findSome? (n := n) fun j =>
-      if mask[j] then
+      if !alreadyTicked[j] then
         match h : states[j] with
         | ⟨m, .xBusWrite pin d next, _, waitingToWrite⟩ =>
           if waitingToWrite[pin] && xBusConns whichPin (j, pin) then
@@ -368,10 +368,10 @@ def setXBusWriteFlags {n : ℕ} (states : Vector State n) : Vector State n :=
     | other => other
 
 /-- Mark everywhere we are `.sleep`ing or `.pure` as having ticked -/
-def setMaskForPureAndSleep {n} (states : Vector State n) (mask : Vector Bool n) : Vector Bool n :=
-  (states.zip mask).map fun (⟨_, is, _, _⟩, b) =>
+def setMaskForPureAndSleep {n} (states : Vector State n) (alreadyTicked : Vector Bool n) : Vector Bool n :=
+  (states.zip alreadyTicked).map fun (⟨_, is, _, _⟩, b) =>
     match is with
-    | .sleep .. | .pure _ => false
+    | .sleep .. | .pure _ => true
     | _ => b
 
 /-! ## What happens in a tick
@@ -410,23 +410,23 @@ partial def advanceTick {n : ℕ}
     (states : Vector State n)
     : Vector State n :=
   go states (Vector.replicate n true)
-  -- TODO I think we can use the bool vector to diagnose programs that never sleep
+  -- TODO I think we can use `alreadyTicked` to diagnose programs that never sleep
 where
-  go (states : Vector State n) (mask : Vector Bool n) : Vector State n :=
-    let (states', mask') := step states mask
-    if mask' == mask then states
-    else go states' mask' -- run until we don't make progress
-  -- TODO show this terminates. I think it can be done with the count of `true` in `mask`
+  go (states : Vector State n) (alreadyTicked : Vector Bool n) : Vector State n :=
+    let (states', alreadyTicked') := step states alreadyTicked
+    if alreadyTicked' == alreadyTicked then states
+    else go states' alreadyTicked' -- run until we don't make progress
+  -- TODO show this terminates. I think it can be done with the count of `false` in `alreadyTicked`
 
-  step (states : Vector State n) (mask : Vector Bool n) : Vector State n × Vector Bool n :=
-    let mask := setMaskForPureAndSleep states mask
+  step (states : Vector State n) (alreadyTicked : Vector Bool n) : Vector State n × Vector Bool n :=
+    let alreadyTicked := setMaskForPureAndSleep states alreadyTicked
 
-    let (states, mask) := resolveXBusReadsAndPeeks xBusConns states mask
+    let (states, alreadyTicked) := resolveXBusReadsAndPeeks xBusConns states alreadyTicked
 
     -- TODO
 
     let states := setXBusWriteFlags states
-    (states, mask)
+    (states, alreadyTicked)
 
 -- TODO somewhere enforce "cannot read pin twice"
 -- TODO test edge case behavior for e.g. `mov x0 x0` or `mov p0 p0`
