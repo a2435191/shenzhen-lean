f769238f0f14f831848ab77fe66c7e23d9c2795f
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-05-27 14:56:48 -0400

    Add back in `isSleep` and `advanceSleep` (further up in the file this time). Delete commented-out proofs

--- Shenzhen/IOEffects.lean
+++ Shenzhen/IOEffects.lean
@@ -41,6 +41,18 @@ namespace IOEffects
 instance [Inhabited α] : Inhabited (IOEffects ξ ι α) :=
   ⟨.pure default⟩
 
+/-- In `sleep` or `xBusPoll` state -/
+abbrev isSleep : IOEffects ξ ι α → Bool
+| .sleep .. | .xBusPoll .. => true
+| _ => false
+
+/-- Advance `.sleep` states by one time unit. Leave `.xBusPoll` states alone. -/
+def advanceSleep (fx : IOEffects ξ ι α) (h : fx.isSleep = true) : IOEffects ξ ι α :=
+  match fx with
+  | .xBusPoll .. => fx
+  | .sleep 1 _ next => .pure next
+  | .sleep (k + 2) _ next => .sleep (k + 1) (Nat.succ_ne_zero _) next
+
 @[simp]
 def map (f : α → β) : IOEffects ξ ι α → IOEffects ξ ι β
   | .pure a => .pure (f a)
@@ -83,69 +95,3 @@ def seq (mf : IOEffects ξ ι (α → β)) (ma : Unit → IOEffects ξ ι α) : 
   | .xBusRead pin next => .xBusRead pin (seq (map Function.swap next) ma)
   | .simpleIORead pin next => .simpleIORead pin (seq (map Function.swap next) ma)
 termination_by sizeOf mf -- hint
-
--- @[simp]
--- def bind (mx : IOEffects ξ ι α) (f : α → IOEffects ξ ι β) : IOEffects ξ ι β :=
---   match mx with
---   | pure a => f a
---   | .xBusRead p next => .xBusRead p fun d => bind (next d) f
---   | .simpleIORead p next => .simpleIORead p fun d => bind (next d) f
---   | .xBusWrite p d next => .xBusWrite p d fun () => bind (next ()) f
---   | .simpleIOWrite p d next => .simpleIOWrite p d fun () => bind (next ()) f
---   | xBusPoll p next => xBusPoll p fun () => bind (next ()) f
---   | .sleep n h next => .sleep n h fun () => bind (next ()) f
-
--- instance : Monad (IOEffects ξ ι) where
---   bind := bind
-
--- section
-
--- -- TODO: make this local
--- attribute [simp] Functor.map Seq.seq Bind.bind Pure.pure
-
--- theorem id_map (x : IOEffects ξ ι α) : id <$> x = x := by
---   induction x <;> try rfl
---   all_goals
---     simp; rename_i ih; funext; apply ih
-
--- theorem bind_pure_comp (f : α → β) (x : IOEffects ξ ι α) : x >>= (fun a => pure (f a)) = f <$> x := by
---   induction x
---   all_goals
---     simp <;> rename_i ih <;> funext <;> apply ih
-
--- instance : LawfulMonad (IOEffects ξ ι) where
---   map_const := rfl
---   id_map := id_map
---   seqLeft_eq x y := by
---     simp [SeqLeft.seqLeft]
---     induction x
---     all_goals
---       first | apply bind_pure_comp | rename_i ih; funext; simp [ih]
---   seqRight_eq x y := by
---     simp [SeqRight.seqRight]
---     induction x
---     all_goals
---       first | symm; apply id_map | rename_i ih; funext; simp [ih]
---   pure_seq f x := by simp [Seq.seq]
---   bind_pure_comp := bind_pure_comp
---   bind_map f x := by
---     induction f <;> simp <;> (rename_i ih; funext; apply ih)
---   pure_bind := by simp
---   bind_assoc x f g := by
---     induction x <;> simp <;> (rename_i ih; funext; apply ih)
-
--- end
-
--- /-- In `sleep` or `xBusPoll` state -/
--- abbrev isSleep : IOEffects ξ ι α → Bool
--- | .sleep .. | .xBusPoll .. => true
--- | _ => false
-
--- /-- Advance `.sleep` states by one time unit. Leave `.xBusPoll` states alone. -/
--- def advanceSleep (fx : IOEffects ξ ι α) (h : fx.isSleep = true) : IOEffects ξ ι α :=
---   match fx with
---   | .xBusPoll .. => fx
---   | .sleep 1 _ next => next ()
---   | .sleep (k + 2) _ next => .sleep (k + 1) (Nat.succ_ne_zero _) next
-
--- end IOEffects
