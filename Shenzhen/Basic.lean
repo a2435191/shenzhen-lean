2a100f5c6ca2809dd900e4d5a475137736e5687f
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-05-29 22:08:00 -0400

    Make `Conns` a single-field struct instead to help start to encapsulate the `connected` relation

--- Shenzhen/Basic.lean
+++ Shenzhen/Basic.lean
@@ -36,12 +36,12 @@ def lightController : Board 4 :=
     slp 1)
   {
     chips := #v[touch, chip₁, chip₂, light],
-    simpleIOConns := .symmetrize fun
+    simpleIOConns := .symmetrize ⟨fun
       | (0, 0), (1, 0) | (2, 1), (3, 1) => true
-      | _, _ => false
-    xBusConns := .symmetrize fun
+      | _, _ => false⟩
+    xBusConns := .symmetrize ⟨fun
       | ⟨1, 1⟩, ⟨2, 0⟩ => true
-      | _, _ => false
+      | _, _ => false⟩
   }
 
 def rep (n : ℕ) (f : α → α) : α → α :=
--- Shenzhen/Conns.lean
+++ Shenzhen/Conns.lean
@@ -1,13 +1,13 @@
 import Shenzhen.Notation
 import Batteries.Data.List.Basic
 
-abbrev Conns (nChips : ℕ) (connType : Type) :=
-  (Fin nChips × connType) → (Fin nChips × connType) → Bool
+structure Conns (nChips : ℕ) (connType : Type) where
+  connected : (Fin nChips × connType) → (Fin nChips × connType) → Bool
 
 namespace Conns
 
 def symmetrize (conns : Conns n c) : Conns n c :=
-  fun a b => conns a b || conns b a
+  ⟨fun a b => conns.connected a b || conns.connected b a⟩
 
 def neighbors {n m} (conns : Conns n (Fin m)) (i : Fin n) (j : Fin m) : List (Fin n × Fin m) :=
-  (List.finRange n).product (List.finRange m)|>.filter (conns (i, j))
+  (List.finRange n).product (List.finRange m)|>.filter (conns.connected (i, j))
